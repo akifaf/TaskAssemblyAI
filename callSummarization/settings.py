75e1fd4c7598bc230d479703ad8b70b5693101dc
Fathima Zohara <fathimaakifa35@gmail.com>
2025-02-03 15:45:30 +0530

    settings

--- callSummarization/settings.py
+++ callSummarization/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-s7m2&3(ozpa(6@y-8yqo*jjq=my!us#o+&y*f60%b%5eldjd#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.8.138.146', '0.0.0.0',]
+ALLOWED_HOSTS = ['52.8.138.146', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -133,6 +135,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), 
+]
 
 
 
