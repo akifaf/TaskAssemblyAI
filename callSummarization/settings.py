abe9c0f8d179207e6890262a061ccc4f57bb966b
Fathima Zohara <fathimaakifa35@gmail.com>
2025-02-03 16:55:16 +0530

    settings

--- callSummarization/settings.py
+++ callSummarization/settings.py
@@ -27,8 +27,12 @@ SECRET_KEY = 'django-insecure-s7m2&3(ozpa(6@y-8yqo*jjq=my!us#o+&y*f60%b%5eldjd#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.8.138.146', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['52.8.138.146', '0.0.0.0', '127.0.0.1', 'localhost',]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://52.8.138.146',
+    'https://52.8.138.146',
+]
 
 # Application definition
 
